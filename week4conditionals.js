00584b4dbac7cf7b2fd4f1fb8f7ab159957beb92
Kartik Kaushik <kartikkaushik116@gmail.com>
2022-10-12 01:06:38 +1100

    Add files via upload

---
+++ week4conditionals.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        msg1: true,
+        numb: 12,
+        showPara:true,
+        rndArray: ["hola","hello","bonjour","chow","ni hao"]
+    }
+})
