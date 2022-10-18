f1fde162343b5da4b87cf5d8c36cd8ce71e0776a
Kartik Kaushik <kartikkaushik116@gmail.com>
2022-10-19 05:25:24 +1100

    Add files via upload

---
+++ week1.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        todos:[
+            {title:'walk the dog', checked:true},
+            {title:'call MOM', checked:true},
+            {title:'finish assignment 3', checked:false},
+            {title:'go buy groceries', checked:false},
+        ]
+    }
+})
