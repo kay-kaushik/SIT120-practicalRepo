aa194e43bdf1ca68c13caa8caafa6db3b7614af2
Kartik Kaushik <kartikkaushik116@gmail.com>
2022-10-18 23:15:34 +1100

    Delete week4conditionals.js

--- week4conditionals.js
+++
@@ -1,9 +0,0 @@
-var app = new Vue({
-    el:'#app',
-    data:{
-        msg1: true,
-        numb: 12,
-        showPara:true,
-        rndArray: ["hola","hello","bonjour","chow","ni hao"]
-    }
-})
